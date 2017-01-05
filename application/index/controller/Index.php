7dccdbe5e30c1858f38661a4447d74e78f621a93
bmkj <1163938946@qq.com>
2017-01-05 13:02:10 +0800

    原始文件
    原始文件

---
+++ application/index/controller/Index.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ThinkPHP5.0+整合百度编辑器Ueditor1.4.3.3+
+作者：符工@邦明
+日期：西元二零一七年元月五日
+网址：http://bbs.df81.com/
+不要怀念哥，哥只是个搬运工
+*/
+
+namespace app\index\controller;
+use think\Controller;
+
+class Index extends Controller{
+    public function index(){
+        return $this->fetch();
+    }
+}
